bbad09b263ce5a912a0b5840c9ea007a75a32952
Nanashiia <theohilaire81@gmail.com>
2026-09-17 21:40:57 +0200

    Create preload.js

---
+++ preload.js
@@ -0,0 +1,19 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('raphael', {
+  getSettings: () => ipcRenderer.invoke('settings:get'),
+  setSettings: (partial) => ipcRenderer.invoke('settings:set', partial),
+  ask: (question) => ipcRenderer.invoke('overlay:ask', question),
+  setExpanded: (expanded) => ipcRenderer.invoke('overlay:set-expanded', expanded),
+  hide: () => ipcRenderer.send('overlay:hide'),
+  clearHistory: () => ipcRenderer.send('overlay:clear-history'),
+  resetPosition: () => ipcRenderer.send('overlay:reset-position'),
+  openSettings: () => ipcRenderer.send('open-settings'),
+  quit: () => ipcRenderer.send('quit-app'),
+  openExternal: (url) => ipcRenderer.send('open-external', url),
+  onSettingsUpdated: (callback) => {
+    const listener = (event, settings) => callback(settings);
+    ipcRenderer.on('settings:updated', listener);
+    return () => ipcRenderer.removeListener('settings:updated', listener);
+  }
+});
